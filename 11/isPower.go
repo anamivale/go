352dc75bbd9110819986937347df7bdf47363717
anamivale <muhembelev@gmail.com>
2023-11-17 01:09:45 +0300

    2

---
+++ 11/isPower.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println()
+	} else {
+		num, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		x := 1
+		flag := false
+		for i := 1; i < 15; i++ {
+			x = x * 2
+			if x == num {
+				flag = true
+			}
+		}
+		if flag == true {
+			fmt.Println("true")
+		} else {
+			fmt.Println("false")
+		}
+	}
+}
